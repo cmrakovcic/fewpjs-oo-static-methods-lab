a0eb48ed99738cd1ab24ca9ece56a052b299fd5c
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-02 14:29:58 -0400

    commit tree 226cba9e6ee4de2d4baccbb9c9487411f845c6ab

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    string = string.replace(/[^a-zA-Z0-9'\. -]/g, '')
+    return string
+  }
+  static titleize(string) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    string = string[0].toUpperCase() + string.slice(1)
+    const array = string.split(" ")
+    for(let n = 0; n < array.length; n++){
+      if(!(exceptions.includes(array[n]))){
+        array[n] = array[n][0].toUpperCase() + array[n].slice(1)
+      }
+    }
+    return array.join(" ")
+  }
 }
